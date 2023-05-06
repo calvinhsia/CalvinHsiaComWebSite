3e54a1d7d15a6137b7048a2370fe714caf01e203
Calvin Hsia <calvinh@microsoft.com>
2023-05-06 13:43:57 -0700

    resize treemap

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,10 +3,22 @@ using BlazorBasic;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+internal class Program
+{
+    public static WebAssemblyHost? Host { get; private set; }
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        
+        Host = builder.Build();
+        //var cl = Program.Host!.Services.GetService<HttpClient>();
+        //var addr = "https://calvinhvscode.azurewebsites.net/api/GetWordData";
+        //var res = await cl!.GetStringAsync(addr);
 
-await builder.Build().RunAsync();
+        await Host.RunAsync();
+    }
+}
