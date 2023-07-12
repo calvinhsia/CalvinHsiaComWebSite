79eb70db4c42ce1beda83b5616bca3cbac57b8ed
Calvin Hsia <calvinh@microsoft.com>
2023-07-12 16:49:08 -0700

    pquery page

---
+++ Api/MyPixDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Client.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public class MyPixWebDBContext : DbContext
+    {
+        public virtual DbSet<MyPix> MyPixes { get; set; }
+        public virtual DbSet<Thumbs> Thumbs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //var fname = @"C:\Users\calvinh\source\repos\CalvinHsiaComWebSite\Api\MyPix.db";
+            var fname = "MyPix.db";
+            if (!File.Exists(fname))
+            {
+                throw new FileNotFoundException(fname);
+            }
+            optionsBuilder.UseSqlite($"Filename={fname}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyPix>(entity =>
+            {
+                entity.ToTable("MyPix"); // needed for sqllite and sqllocaldb : map MyPixes=>MyPix
+                entity.Property(e => e.Date).HasColumnType("datetime");
+                entity.Property(e => e.FileName)
+                    .HasMaxLength(255)
+                    .IsUnicode(true);
+                entity.Property(e => e.Notes)
+                    .HasMaxLength(250)
+                    .IsUnicode(true);
+            });
+        }
+    }
+}
