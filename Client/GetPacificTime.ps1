5bf2f9f1b7a4b43cc394300432ca5c195d79c31c
Calvin Hsia (from Dev Box) <calvinh@microsoft.com>
2025-12-01 12:46:47 -0800

    use C# to get tzone

--- Client/GetPacificTime.ps1
+++
@@ -1,5 +0,0 @@
-# Get current time in Pacific Time Zone (handles PST/PDT automatically)
-$pacificZone = [System.TimeZoneInfo]::FindSystemTimeZoneById('Pacific Standard Time')
-$pacificTime = [System.TimeZoneInfo]::ConvertTimeFromUtc([System.DateTime]::UtcNow, $pacificZone)
-$abbreviation = if ($pacificZone.IsDaylightSavingTime($pacificTime)) { "PDT" } else { "PST" }
-Write-Output "$($pacificTime.ToString('yyyy-MM-dd HH:mm:ss')) $abbreviation"
